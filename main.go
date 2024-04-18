6f412ced27530a171390946f3102999d07a7b85c
sabrina-djebbar <95046641+sabrina-djebbar@users.noreply.github.com>
2024-04-18 17:47:32 +0100

    create-user

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type CreateUserRequest struct {
+	Username    string    `json:"username"`
+	Password    string    `json:"password"`
+	ParentCode  string    `json:"parent_code"`
+	DateOfBirth time.Time `json:"date_of_birth"`
+}
+type User struct {
+	Id          int
+	Username    string
+	ParentCode  string
+	DateOfBirth time.Time
+}
+
+func main() {
+	connStr := "postgres://postgres:secret@localhost:5432/spelling-app?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	// create tables
+	createCredentialsTable(db)
+	createUserTable(db)
+
+	userReq := CreateUserRequest{"user", "password", "1234", time.Now()}
+	user := createUser(db, userReq)
+	fmt.Print("user ", user)
+}
+
+func createCredentialsTable(db *sql.DB) {
+	_, err := db.Exec("CREATE EXTENSION IF NOT EXISTS pgcrypto")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlQuery := `CREATE TABLE IF NOT EXISTS credentials (
+		id SERIAL PRIMARY KEY,
+		user_id SERIAL,
+		password VARCHAR(50),
+		FOREIGN KEY (user_id) REFERENCES users(id)
+	);`
+	_, err = db.Exec(sqlQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createUserTable(db *sql.DB) {
+	sqlQuery := `CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		username VARCHAR(50) NOT NULL,
+		parent_code VARCHAR(4),
+		date_of_birth DATE,
+		created timestamp DEFAULT NOW()
+	);`
+
+	_, err := db.Exec(sqlQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createUser(db *sql.DB, req CreateUserRequest) int {
+	sqlUserQuery := `INSERT INTO users (username, date_of_birth, parent_code) VALUES ($1, $2, $3) RETURNING id, username`
+	sqlCredentialQuery := `INSERT INTO credentials (user_id, password) VALUES($1,crypt($2,'crypt-des'));`
+	var (
+		id       int
+		username string
+	)
+	err := db.QueryRow(sqlUserQuery, req.Username, req.DateOfBirth, req.ParentCode).Scan(&id, &username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(sqlCredentialQuery, id, req.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("user ", id, username)
+	return id
+}
